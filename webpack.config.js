da016ea87d6608c14588a079939a5618fb183508
simon-hall-970 <60405692+simon-hall-970@users.noreply.github.com>
2020-07-23 16:50:55 +1200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+      test:/\.css$/,
+      use: [{
+        loader: "style-loader"
+      },
+      {
+        loader: "css-loader"
+      }]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './public'
+  }
+}
