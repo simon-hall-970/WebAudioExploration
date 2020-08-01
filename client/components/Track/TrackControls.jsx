de7cb5ae50ea02fb81edce0bc2df9883dae49fe8
Simon Hall <simon970@gmail.com>
2020-08-01 20:28:15 +1200

    make pause button suspend audio context

--- client/components/Track/TrackControls.jsx
+++ client/components/Track/TrackControls.jsx
@@ -8,7 +8,8 @@ class TrackControls extends React.Component {
 
     state = {
         play: true,
-        track: `track${this.props.track}` //it's ok. props.track will not be updated so this is perfectly legit
+        track: `track${this.props.track}`, //it's ok. props.track will not be updated so this is perfectly legit
+        isPlaying: false
     }
     
 
@@ -38,8 +39,25 @@ class TrackControls extends React.Component {
         let division = this.props.measure[0].subdivision
         let beatVal = this.props.measure[0].beatValue
         let noteCount = this.props.notes.track1[this.props.notes.track1.length-1].note
-        const schedulerInterval = setInterval(() => {
+        
+        if(this.state.isPlaying == true){
+            this.setState({
+                isPlaying: false
+            })
+            audioCtx.suspend()
+            clearInterval(schedulerInterval)
+ 
+        }else {
+            this.setState({
+                isPlaying: true
+            })
+            if(audioCtx.state === 'suspended'){
+                audioCtx.resume()
+            }
+            const schedulerInterval = setInterval(() => {
             noteScheduler(this.props.notes, this.props.kit.track1)}, 25)
+        }
+
     }
 
     render() {
@@ -49,7 +67,7 @@ class TrackControls extends React.Component {
                     <button className = 'btn load' onClick={this.loadSample} value='snare.wav'>snare</button>
                     <button className = 'btn play' onClick={this.play} value="snare" >Test</button>
                     <button className = 'btn playPause' disabled={this.state.play} onClick={this.playPause}>
-                    ►
+                    {this.state.isPlaying ? '‖' : '►'}
                     </button>
                 </div>
         )
