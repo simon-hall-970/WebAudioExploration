49a2147cfeb2e89459d08f71e9290f8800ca2e4b
Simon Hall <simon970@gmail.com>
2020-09-15 16:08:05 +1200

    update slider css

--- client/components/Track/TrackVolume.jsx
+++ client/components/Track/TrackVolume.jsx
@@ -15,8 +15,9 @@ class TrackVolume extends React.Component {
     render () {
         return (
             <div className="track-vol-wrapper">
-                <input id="trackVolume" type="range" max="100" min="0" step='1' onChange={this.volumeHandler} />
                 <FontAwesomeIcon className="track-volume-icon" icon={faVolumeUp} />
+                <input id="trackVolume" type="range" max="100" min="0" step='1' onChange={this.volumeHandler} />
+                
             </div>
         )
     }
